d10c7cd4dd0d9e6f9137f13731a0d6ace1fb67e6
Kevin Diem <kg.diem@gmail.com>
2020-04-05 22:22:27 -0400

    Add boilerplate to entries module

--- apps/poo-diary/src/app/core/configs/routes.ts
+++ apps/poo-diary/src/app/core/configs/routes.ts
@@ -4,5 +4,10 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('../../home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'entries',
+    loadChildren: () =>
+      import('../../entries/entries.module').then(m => m.EntriesModule)
   }
 ];
--- apps/poo-diary/src/app/entries/entries.module.ts
+++ apps/poo-diary/src/app/entries/entries.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListComponent } from './list/list.component';
-
-
+import { ListComponent } from './pages/list/list.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './configs';
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes)]
 })
-export class EntriesModule { }
+export class EntriesModule {}
